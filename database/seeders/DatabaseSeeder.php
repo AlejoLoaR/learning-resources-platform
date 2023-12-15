c555e2c5c6817d3acf21a401eb875c3c1274bf82
Alejandro <loaizaramirezalejandro@gmail.com>
2023-12-15 09:28:55 -0500

    Creating Database Migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,23 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Resource;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run():  void
     {
-        // \App\Models\User::factory(10)->create();
+       $categories = Category::factory()->count(5)->create();
+        foreach ($categories as $category){
+            Resource::factory()->count(5)->create([
+                'category_id' =>$category->id,
+            ]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
